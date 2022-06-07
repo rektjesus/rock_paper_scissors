46a4a8bd54861c1e8a44223c01264f4d96e6d2a6
sleepless <arnas.manucan@gmail.com>
2022-06-07 14:32:39 +0300

    Create random number function

---
+++ script.js
@@ -0,0 +1,12 @@
+const arr = ["rock", "paper", "scissors"];
+let computerChoice;
+let userChoice;
+
+function randomNumber(max) {
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function computerPlay(arr) {
+
+}
